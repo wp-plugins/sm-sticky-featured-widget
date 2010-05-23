0739242f2217992fd4b28cd0b242a4bf80fe05ac
sethmatics <sethmatics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-23 11:14:09 +0000

    deploying version 1.0.1 and closing development on this version.
    git-svn-id: https://plugins.svn.wordpress.org/sm-sticky-featured-widget/tags/1.0.1@244729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== SM Sticky Featured Widget ===
+
+Contributors: Sethmatics Inc feat. Seth Carstens
+Donate link: http://smwphosting.com/extend
+Tags: widgets, sticky, featured, classipress
+Requires at least: 2.7
+Tested up to: 3.0-beta2-14697
+Stable tag: 1.0.1
+
+A tiny but high in demand widget to post sticky or "featured" posts into any widget area. Widget provided by http://smwphosting.com/.
+
+== Description ==
+
+A tiny but high in demand widget to post sticky or "featured" posts into any widget area. We would appriciate any assistance you would like to provide with coding of enhancments to this plugin.
+
+Features include:
+
+* sidebar widget added to your "Widgets" appearance settings that shows only sticky posts.
+* option to set the title when displayed on the home page.
+* Now COMPLETELY compatable with the premium "ClassiPress" theme which can be purchased at [ClassiPress](http://wpclassipress.com/ "www.wpclassipress.com")
+* demo of activated widget available at http://cpmodlite.smwphosting.com/
+* uses the WP_Query to efficiently grab your sticky posts
+* makes use of new thumbnails and "featured images" for wordpress 2.9.2 and wordpress 3.0
+
+
+Features Coming in Version 2:
+
+* option to disable display of the title, or always show a static title instead of the forced category titles.
+* option to set the number of posts listed
+* option to turn on and off the thumbnails
+
+
+Don't forget to rate our plugin so we know how we are doing!
+
+== Installation ==
+
+To install the plugin manually:
+
+1. Extract the contents of the archive (zip file)
+2. Upload the sm-sticky-featured-widget folder to your '/wp-content/plugins' folder
+3. Activate the plugin through the Plugins section in your WordPress admin
+4. Find and "click and drag" the newly created widget to your sidebar and set the options.
+
+== Changelog ==
+- Heavily modified the queries and is now 100% ClassiPress compliant.
+- Version 1.0.0 is the original released version. No changes logged yet.
+
+== Upgrade Notice ==
+- Version 1.0.1 requires no special upgrade instructions.
+
+= 1.0.1 =
+Rewrote all the query code to properly display only featured sticky posts and also allows the "ClassiPress" theme properly exclude all blog categories.
+= 1.0.0 =
+Created the option to set the title when shown on the homepage (subpages show category names which will be optional in the future)
+Registered the Widget (recurring so you can use it multiple times)
+
+== Frequently Asked Questions ==
+
+Q: How do I make a sticky post?
+A: When you publish a post if you look under the "visibility" options you will see an option to make your post sticky.
+
+Q: What is a sticky post?
+A: Typically they are posts that you want to "always show on top" of your blog posts regardless of the post date.
+   In some special cercumstances like ClassiPress, sticky is used to create a "featured" section of the website.
+
+Q: How do I use the plugin?
+A: Simple, just install it from your WP-Admin->Plugins page, then you will find a "widget" from the appearance section that you can drag
+   into any of your sidebar areas. The exact placement will greatly change depending on your selected theme.
+
+== Screenshots ==
+
+1. A sample of the widget when used on premium theme like ClassiPress.
+2. 
---
+++ sticky-featured-widget.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Plugin Name: SM Sticky Featured Widget
+Plugin URI: http://www.smwphosting.com/extend/plugins/sm-sticky-widget
+Description: A tiny but high in demand widget to post sticky or "featured" posts into any widget area.
+Author: Seth Carstens
+Version: 1.0.0
+Author URI: http://www.smwphosting.com/
+*/
+
+class WP_Widget_smSticky extends WP_Widget {
+
+	// The widget construct instatiates the widget
+	function WP_Widget_smSticky() {
+		$widget_ops = array( 'classname' => 'widget_smSticky', 'description' => __( "SM Sticky Featured Posts" ) );
+		$this->WP_Widget('smSticky', __('SM Sticky Posts'), $widget_ops);
+	} // End function WP_Widget_smSticky
+
+	// This code displays the widget on the screen.
+	function widget($args, $instance) {
+		extract($args);
+		echo $before_widget;
+
+		//Building The Query
+		
+		//if the blog category is set, get it and set it to a negative number to exclude it
+		if(get_option('cp_blog_cat')) $blogcatexclude = (int)CP_BLOG_CAT_ID * (-1);
+		else $blogcatexclude = '';
+		
+		//getting the category depending on the type of page we are on
+		if(is_single()) {
+			$categories = get_the_category($post->ID);
+			$get_cat_name = $categories[0]->cat_name; 
+			$get_cat_id = $categories[0]->cat_ID;
+		}
+		else
+		{
+			$get_cat_name = single_cat_title("", false);
+			$get_cat_id = get_cat_ID($get_cat_name);
+		}
+		
+		//if get cat ID returned nothing (zero) or we are on the home page, use defined title and exclude blog
+		if($get_cat_id == 0 || is_home()) {
+			if(isset($instance['title'])) echo $before_title . $instance['title'] . $after_title;
+			else  echo $before_title . __('Sticky Posts', 'cp') . $after_title;
+			
+			//CLASSIPRESS ONLY grab a string of all the blog categories.
+			$blog_cats_string = (string)CP_BLOG_CAT_ID;
+			$blog_cat_args = array(
+				'type'                     => 'post',
+				'child_of'                 => CP_BLOG_CAT_ID,
+				'pad_counts'               => false );
+			$blog_categories = get_categories($blog_cat_args);
+			foreach ($blog_categories as $cat) {
+				$blog_cats_string .= "," . $cat->cat_ID;
+			}
+			
+			//grab all the categories and exclude the blog categories
+			$cp_cat_args = array(
+				'type'                     => 'post',
+				'child_of'                 => 0,
+				'orderby'                  => 'name',
+				'order'                    => 'ASC',
+				'hide_empty'               => 1,
+				'hierarchical'             => 1,
+				'exclude'                  => $blog_cats_string,
+				'pad_counts'               => false );
+			
+			$cp_categories = get_categories($cp_cat_args);
+			foreach ($cp_categories as $cat) {
+				$cp_cats_string .= $cat->cat_ID . ",";
+			}
+
+			$argsqry = array('cat' => $cp_cats_string);	
+		}
+		//if no error and not on the home page, we must be in a category, display that title instead.
+		else {
+			echo $before_title . __('Featured in ') . $get_cat_name . $after_title;	
+			$argsqry = array('cat' => $categories[0]->cat_ID);
+		}
+		
+		//always use these attributes when loading the query
+		$queryArrayOrString = array(
+			'post__in'  => get_option('sticky_posts'),
+			'posts_per_page' => 5,
+			'orderby' => 'rand',
+			'post_status' => 'publish');
+
+		if(isset($argsqry)) $queryArrayOrString = array_merge($argsqry, $queryArrayOrString);
+		$smStickyPosts = new WP_Query($queryArrayOrString);
+
+		//The Loop
+		while ($smStickyPosts->have_posts()) : $smStickyPosts->the_post();
+			if (has_post_thumbnail()){
+				echo '<a href="' . get_permalink() . '">';
+				the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('class' => 'alignleft'));
+				echo '</a>';
+			}
+			elseif(function_exists('cp_get_image_url_feat')) cp_get_image_url_feat(get_the_id(), 'thumbnail', 'captify', 1); 
+			echo '<h3><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>';
+			echo '<br style="clear: both;" />';
+		endwhile;
+		//End of The Loop
+		
+		//Reset Query
+		//wp_reset_query();
+		//echo '<br /><br />Debug of Query Array Arguments Passed: <br />';
+		//var_dump($queryArrayOrString);
+		
+		echo $after_widget;
+	} // End function widget.
+	
+	
+	
+	// Updates the settings.
+	function update($new_instance, $old_instance) {
+		return $new_instance;
+	} // End function update
+	
+	
+	
+	// The admin form.
+	function form($instance) {		
+		echo '<div id="smSticky-admin-panel">';
+		echo '<label for="' . $this->get_field_id("title") .'">smSticky Title:</label>';
+		echo '<input type="text" class="widefat" ';
+		echo 'name="' . $this->get_field_name("title") . '" '; 
+		echo 'id="' . $this->get_field_id("title") . '" ';
+		echo 'value="' . $instance["title"] . '" />';
+		echo '<p>This widget will display the title you choose above followed by posts with the "sticky" published option enabled.</p>';
+		echo '</div>';
+	} // end function form
+
+} // end class WP_Widget_smSticky
+
+// Register the widget.
+add_action('widgets_init', create_function('', 'return register_widget("WP_Widget_smSticky");'));
+
+?>
