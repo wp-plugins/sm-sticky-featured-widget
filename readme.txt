323f03f76b0f0789a727d14265780c23e58dc81a
sethmatics <sethmatics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-05 08:09:09 +0000

    updated plugin for tag out, fixed plugin for users who upgraded to the newest version of ClassiPress, currently version 3.0.5.3
    git-svn-id: https://plugins.svn.wordpress.org/sm-sticky-featured-widget/tags/1.1.0@328926 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,89 @@
+=== SM Sticky Featured Widget ===
+
+Contributors: Sethmatics Inc feat. Seth Carstens
+Donate link: http://sethmatics.com/extend/plugins/sm-sticky-widget
+Tags: widgets, sticky, featured, classipress
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: 1.1.0
+
+A tiny but high in demand widget to post sticky or "featured" posts into any widget area. Widget provided by http://smwphosting.com/.
+
+== Description ==
+
+A tiny but high in demand widget to post sticky or "featured" posts into any widget area. We would appriciate any assistance you would like to provide with coding of enhancments to this plugin.
+Using Documentation from core trac default widgets as a guideline for development:
+http://core.trac.wordpress.org/browser/trunk/wp-includes/default-widgets.php
+
+Features include:
+
+* Now COMPLETELY compatable with the premium "ClassiPress" theme which can be purchased at [ClassiPress](http://appthemes.com/cp/go.php?r=2505&i=b3  "www.appthemes.com")
+* sidebar widget added to your "Widgets" appearance settings that shows only sticky posts.
+* option to set the title when displayed on the home page.
+* makes use of new thumbnails and "featured images" for wordpress 2.9.2 and wordpress 3.0
+* option to turn on and off the thumbnails
+* option to set the number of posts listed (upgraded and not dependent on wordpress query options bug)
+* option to show "Category Related Sticky Posts" when sidebar displays on category page
+* detect if no featured ads are in category/subcategories and post any featured posts/pages.
+
+Features Coming in version 2 Next Version Updates:
+
+* option to choose a custom post type to display sticky ads for (instead of only compliant with ClassiPress).
+* option to set the width and height of thumbnail inline css for non-classipress themes.
+* option to disable display of the title, or always show a force title option text regardless of website page type.
+* option to specify "only included" categoires or to "exclude only" certain categories.
+* option to "only feature listed post / page ID's" in the widget
+* option to turn off the "random" order listing and choose from a dropdown of order types.
+* option to limit the title to X number of characters
+* option to display exceprt up to X number of characters or default to the wordpress excerpt words and length configured.
+* option to enable a jQuery based veritcle sidebar carousel (exact model to be determined).
+
+Don't forget to rate our plugin so we know how we are doing!
+
+== Installation ==
+
+To install the plugin manually:
+
+1. Extract the contents of the archive (zip file)
+2. Upload the sm-sticky-featured-widget folder to your '/wp-content/plugins' folder
+3. Activate the plugin through the Plugins section in your WordPress admin
+4. Find and "click and drag" the newly created widget to your sidebar and set the options.
+
+== Changelog ==
+- Updated for custom post types used in ClassiPress.
+- Heavily modified the queries and is now 100% ClassiPress compliant.
+
+== Upgrade Notice ==
+Version 1.1.0
+- Compatable with ClassiPress version 3.0.5.X and 3.1
+- Fixed homepage number of displayed ads bug
+
+Version 1.0.2
+- Adding the ability to "include" only certain categories OR exclude only certain categories.
+- Added the ability to turn on or off thumbnails for the 
+- Added the option to set the number of posts listed
+
+Version 1.0.1
+Rewrote all the query code to properly display only featured sticky posts and also allows the "ClassiPress" theme properly exclude all blog categories.
+
+Version 1.0.0
+Created the option to set the title when shown on the homepage (subpages show category names which will be optional in the future)
+Registered the Widget (recurring so you can use it multiple times)
+
+== Frequently Asked Questions ==
+
+Q: How do I make a sticky post?
+A: When you publish a post if you look under the "visibility" options you will see an option to make your post sticky.
+
+Q: What is a sticky post?
+A: Typically they are posts that you want to "always show on top" of your blog posts regardless of the post date.
+   In some special cercumstances like ClassiPress, sticky is used to create a "featured" section of the website.
+
+Q: How do I use the plugin?
+A: Simple, just install it from your WP-Admin->Plugins page, then you will find a "widget" from the appearance section that you can drag
+   into any of your sidebar areas. The exact placement will greatly change depending on your selected theme.
+
+== Screenshots ==
+
+1. A sample of the widget when used on premium theme like ClassiPress.
+2. A sample of the widget on the default Wordpress theme, TwentyTen.
---
+++ sticky-featured-widget.php
@@ -0,0 +1,170 @@
+<?php
+/*
+Plugin Name: SM Sticky Featured Widget
+Plugin URI: http://www.smwphosting.com/extend/plugins/sm-sticky-widget
+Description: A tiny but high in demand widget to post sticky or "featured" posts into any widget area complient with ClassiPress.
+Author: Seth Carstens
+Version: 1.1.0
+Author URI: http://www.smwphosting.com/
+*/
+
+// Register the widget.
+add_action('widgets_init', create_function('', 'return register_widget("WP_Widget_smSticky");'));
+
+class WP_Widget_smSticky extends WP_Widget {
+
+	// The widget construct instatiates the widget
+	function WP_Widget_smSticky() {
+		$widget_ops = array( 'classname' => 'widget_smSticky', 'description' => __( "SM Sticky Featured Posts" ) );
+		$this->WP_Widget('smSticky', __('SM Sticky Posts'), $widget_ops);
+	} // End function WP_Widget_smSticky
+
+	// This code displays the widget on the screen.
+	function widget($args, $instance) {
+		wp_reset_query();
+		extract($args);
+		
+		//detect and set configuration for classipress or standard wordpress theme
+		if(post_type_exists('ad_listing')) { $cp = true; $postType = 'ad_listing'; $catType = 'ad_cat'; }
+		else { $cp = false; $postType = 'post'; $catType = 'category'; }
+		
+		if(get_query_var('taxonomy')) {
+			$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+		}
+		elseif(is_category()) {
+			$term = get_term_by('id', get_cat_ID(single_cat_title("", false)), 'category');
+		}
+		elseif(is_single()) { //single page
+			$terms = wp_get_post_terms(get_the_ID(), $catType); //print_r($terms); exit;
+			$term = $terms[0];
+		}
+
+		/* User-selected settings. */
+		//title
+		$title = apply_filters('widget_title', $instance['title'] );
+		//number of posts (if never saved, defaults to 5
+		if ( !$number = (int) $instance['number'] ) $number = 5;
+		else if ( $number < 1 ) $number = 1;
+		else if ( $number > 15 ) $number = 15;
+		//thumbnails
+		$showthumbs = isset( $instance['showthumbs'] ) ? $instance['showthumbs'] : false;
+		$catTitles = isset( $instance['catTitles'] ) ? $instance['catTitles'] : false;
+		
+		//determine the sticky post type (custom post types)
+		if(post_type_exists('ad_listing')) $postType = 'ad_listing';
+		else $postType = 'post';
+		
+		//always use these attributes when loading the query
+		$queryArrayOrString = array(
+			'post__in'  => get_option('sticky_posts'),
+			'post_type' => $postType,
+			'posts_per_page' => $number,
+			'orderby' => 'rand',
+			'post_status' => 'publish');
+
+		//if there is a term, only display featured ads from that term assuming the option is turned on
+		if($term->slug && $catTitles) {
+			if($cp) $queryArrayOrString['ad_cat'] = $term->slug;
+			else $queryArrayOrString['category_name'] = $term->slug;
+		}
+		$smStickyPosts = new WP_Query($queryArrayOrString);
+		
+		//start printing the widget
+		echo $before_widget;
+		
+		//if not on the home page, we must be in a category, display that title instead.
+		if( !is_home() && $catTitles) echo $before_title . __('Featured in ') . $term->name . $after_title;	
+		//means this is the home page : use defined title and exclude blog category ID's
+		else {
+			if(isset($instance['title'])) echo $before_title . $instance['title'] . $after_title;
+			else  echo $before_title . __('Sticky Posts', 'cp') . $after_title;			
+		}
+		
+		//The Loop (modified)
+		echo '<ul class="featured-sidebar">';
+		$i = 0;
+		if(!$smStickyPosts->have_posts()) echo 'No Featured Posts Found';
+		while ($smStickyPosts->have_posts() && ($i < $smStickyPosts->query_vars['posts_per_page'])) : $smStickyPosts->the_post();
+			if(post_type_exists('ad_listing')) : 
+		?>
+			<li>
+				<div class="post-thumb" style="min-width:50px;">
+					<?php if(function_exists('cp_ad_featured_thumbnail') && $showthumbs) cp_ad_featured_thumbnail();
+				elseif (has_post_thumbnail() && $showthumbs){
+					echo '<a href="' . get_permalink() . '">';
+					the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('class' => 'alignleft'));
+					echo '</a>';
+				}	 ?>
+				</div>
+				<h3><a href="<?php the_permalink(); ?>"><?php if (mb_strlen(get_the_title()) >= 40) echo mb_substr(get_the_title(), 0, 40).'...'; else the_title(); ?></a></h3>
+				<p class="side-meta"><span class="folder"><?php if (get_the_category()) the_category(', '); else echo get_the_term_list($post->ID, 'ad_cat', '', ', ', ''); ?></span> | <?php if(get_post_meta(get_the_ID(), 'price', true)) cp_get_price_legacy(get_the_ID()); else cp_get_price(get_the_ID()); ?></p>
+				<p><?php echo mb_substr(strip_tags(get_the_content()), 0, 160).'...';?></p>
+			</li>
+			<?php
+			else :	
+				echo '<li>';
+				if(function_exists('cp_ad_featured_thumbnail') && $showthumbs) cp_ad_featured_thumbnail();
+				elseif (has_post_thumbnail() && $showthumbs){
+					echo '<a href="' . get_permalink() . '">';
+					the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('style' => 'float: left; width: 50px; height: 50px; margin-right: 5px;'));
+					echo '</a>';
+				}		
+				echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
+				echo '<p style="margin: 0px;">'.mb_substr(strip_tags(get_the_content()), 0, 160).'...'.'</p>';
+				echo '<br style="clear: both;" />';
+				echo '</li>';
+			endif;
+			$i++;
+		endwhile;
+		echo '</ul>';
+		//End of The Loop
+		echo $after_widget;
+		
+		//debug printing
+		//echo 'query_vars <pre>'; print_r($smStickyPosts->query_vars); echo '</pre>';
+		//echo '$queryArrayOrString <pre>'; print_r($queryArrayOrString); echo '</pre>';
+		//echo '$term <pre>'; print_r($term); echo '</pre>';
+		wp_reset_query();
+	} // End function widget.
+	
+	
+	
+	// Updates the settings.
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		/* Strip tags (if needed) and update the widget settings. */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number'] = (int) $new_instance['number'];
+        $instance['showthumbs'] = isset($new_instance['showthumbs']) ? 1 : 0;
+        $instance['catTitles'] = isset($new_instance['catTitles']) ? 1 : 0;
+		return $instance;
+	} // End function update
+	
+	
+	
+	// The admin form.
+	function form($instance) {	
+	$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+	if ( !isset($instance['number']) || !$number = (int) $instance['number'] ) $number = 5;
+?>
+<div id="smSticky-admin-panel">
+    <p><label for="<?php echo $this->get_field_id("title"); ?>">Title:</label>
+    <input type="text" class="widefat" name="<?php echo $this->get_field_name("title"); ?>" id="<?php echo $this->get_field_id("title"); ?>" value="<?php echo $instance["title"]; ?>" /></p>
+
+	<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
+	<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+
+    <p><input class="checkbox" type="checkbox" <?php checked( $instance['showthumbs'], true ); ?> id="<?php echo $this->get_field_id( 'showthumbs' ); ?>" name="<?php echo $this->get_field_name('showthumbs'); ?>" />
+    <label for="<?php echo $this->get_field_id('showthumbs'); ?>"> Display Thumbnails?</label></p>
+    
+        <p><input class="checkbox" type="checkbox" <?php checked( $instance['catTitles'], true ); ?> id="<?php echo $this->get_field_id( 'catTitles' ); ?>" name="<?php echo $this->get_field_name('catTitles'); ?>" />
+    <label for="<?php echo $this->get_field_id('catTitles'); ?>"> Only show sticky posts of the post category & sub categories?</label></p>
+    
+    </div>
+    
+<?php
+	} // end function form
+
+} // end class WP_Widget_smSticky
+
+?>
