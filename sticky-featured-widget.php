634614610cbecdf9e0d6b813424b5d6917239ae7
sethcarstens <sethcarstens@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-28 01:18:11 +0000

    Created tag 1.2.5.
    git-svn-id: https://plugins.svn.wordpress.org/sm-sticky-featured-widget/tags/1.2.5@1207918 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky-featured-widget.php
+++ sticky-featured-widget.php
@@ -4,7 +4,7 @@ Plugin Name: SM Sticky Featured Widget
 Plugin URI: http://sethmatics.com/extend/plugins/sm-sticky-widget
 Description: A tiny but high in demand widget to post sticky or "featured" posts into any widget area complient with ClassiPress. 
 Author: sethcarstens
-Version: 1.2.4
+Version: 1.2.5
 Author URI: http://sethmatics.com/
 */
 
@@ -13,11 +13,30 @@ add_action('widgets_init', create_function('', 'return register_widget("WP_Widge
 
 class WP_Widget_smSticky extends WP_Widget {
 
-	// The widget construct instatiates the widget
-	function WP_Widget_smSticky() {
-		$widget_ops = array( 'classname' => 'widget_smSticky', 'description' => __( "SM Sticky Featured Posts" ) );
-		$this->WP_Widget('smSticky', __('SM Sticky Posts'), $widget_ops);
-	} // End function WP_Widget_smSticky
+	//used to store defaults for easy parsing during admin form creation
+	protected $defaults;
+
+	//init options and defaults for widget admin area
+	function __construct(){
+		//setup default widget data
+		$this->defaults = array(
+			'title'         => '',
+			'number'        => 5,
+			'showthumbs'    => 1,
+			'catTitles'   	=> 1,
+			'showType' 		=> 'sticky',
+		);
+		//setup default widget options
+		parent::__construct(
+			'WP_Widget_smSticky',
+			esc_html__( 'SM Sticky Posts', 'sm-sticky-widget' ),
+			array(
+				'classname'   => 'widget_smStickyt',
+				'description' => __( 'SM Sticky Featured Posts', 'sm-sticky-widget' ),
+			)
+		);
+
+	}
 
 	// This code displays the widget on the screen.
 	function widget($args, $instance) {
@@ -28,7 +47,7 @@ class WP_Widget_smSticky extends WP_Widget {
 		//detect and set configuration for classipress or standard wordpress theme
 		if(post_type_exists('ad_listing')) { $cp = true; $postType = 'ad_listing'; $catType = 'ad_cat'; }
 		else { $cp = false; $postType = 'post'; $catType = 'category'; }
-		
+
 		if(get_query_var('taxonomy')) {
 			$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
 		}
@@ -50,11 +69,11 @@ class WP_Widget_smSticky extends WP_Widget {
 		//thumbnails
 		$showthumbs = isset( $instance['showthumbs'] ) ? $instance['showthumbs'] : false;
 		$catTitles = isset( $instance['catTitles'] ) ? $instance['catTitles'] : false;
-		
+
 		//determine the sticky post type (custom post types)
 		if(post_type_exists('ad_listing')) $postType = 'ad_listing';
 		else $postType = 'post';
-		
+
 		//always use these attributes when loading the query
 		$queryArrayOrString = array(
 			'post_type' => $postType,
@@ -63,17 +82,17 @@ class WP_Widget_smSticky extends WP_Widget {
 			'post_status' => 'publish');
 		if($instance['showType'] == 'sticky') $queryArrayOrString['post__in'] = get_option('sticky_posts');
 		else if($instance['showType'] == 'reg') $queryArrayOrString['post__not_in'] = get_option('sticky_posts');
-		
+
 		//if there is a term, only display featured ads from that term assuming the option is turned on
 		if($term->slug && $catTitles) {
 			if($cp) $queryArrayOrString['ad_cat'] = $term->slug;
 			else $queryArrayOrString['category_name'] = $term->slug;
 		}
 		$smStickyPosts = new WP_Query($queryArrayOrString);
-		
+
 		//start printing the widget
 		echo $before_widget;
-		
+
 		//if not on the home page, we must be in a category, display that title instead.
 		if( !is_home() && !is_front_page() && $catTitles) {
 			echo $before_title . __('Featured in ') . $term->name . $after_title;
@@ -81,39 +100,39 @@ class WP_Widget_smSticky extends WP_Widget {
 		//means this is the home page : use defined title and exclude blog category ID's
 		else {
 			if(isset($instance['title'])) echo $before_title . $title . $after_title;
-			else  echo $before_title . __('Sticky Posts', 'cp') . $after_title;			
+			else  echo $before_title . __('Sticky Posts', 'cp') . $after_title;
 		}
-		
+
 		//The Loop (modified)
 		echo '<ul class="featured-sidebar">';
 		$i = 0;
 		if(!$smStickyPosts->have_posts()) echo 'No Featured Posts Found';
 		while ($smStickyPosts->have_posts() && ($i < $smStickyPosts->query_vars['posts_per_page'])) : $smStickyPosts->the_post();
-			if(post_type_exists('ad_listing')) : 
-		?>
-			<li>
-				<div class="post-thumb" style="min-width:50px;">
-                <?php if(get_post_meta(get_the_ID(), 'cp_ad_sold', true) == 'yes') : ?><div class="sold-ribbon"></div><?php endif; ?>
-					<?php if(function_exists('cp_ad_featured_thumbnail') && $showthumbs) cp_ad_featured_thumbnail();
-				elseif (has_post_thumbnail() && $showthumbs){
-					echo '<a href="' . get_permalink() . '">';
-					the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('class' => 'alignleft'));
-					echo '</a>';
-				}	 ?>
-				</div>
-				<h3><a href="<?php the_permalink(); ?>"><?php if (mb_strlen(get_the_title()) >= 40) echo mb_substr(get_the_title(), 0, 40).$readmore_text; else the_title(); ?></a></h3>
-				<p class="side-meta"><span class="folder"><?php if (get_the_category()) the_category(', '); else echo get_the_term_list($post->ID, 'ad_cat', '', ', ', ''); ?></span> <?php if(get_post_meta(get_the_ID(), 'cp_ad_sold', true) == 'yes') : echo apply_filters('sticky_featured_sold_text', ' '); else: ?>| <?php echo cp_get_price(get_the_ID(), 'cp_price'); ?>				<?php endif; ?>																							<!--<?php if(get_post_meta(get_the_ID(), 'price', true)) cp_get_price_legacy(get_the_ID()); else cp_get_price(get_the_ID(), 'cp_price'); ?></p>-->
-				<p><?php echo mb_substr(strip_tags(get_the_content()), 0, 80).$readmore_text;?></p>
-			</li>
-			<?php
-			else :	
+			if(post_type_exists('ad_listing')) :
+				?>
+				<li>
+					<div class="post-thumb" style="min-width:50px;">
+						<?php if(get_post_meta(get_the_ID(), 'cp_ad_sold', true) == 'yes') : ?><div class="sold-ribbon"></div><?php endif; ?>
+						<?php if(function_exists('cp_ad_featured_thumbnail') && $showthumbs) cp_ad_featured_thumbnail();
+						elseif (has_post_thumbnail() && $showthumbs){
+							echo '<a href="' . get_permalink() . '">';
+							the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('class' => 'alignleft'));
+							echo '</a>';
+						}	 ?>
+					</div>
+					<h3><a href="<?php the_permalink(); ?>"><?php if (mb_strlen(get_the_title()) >= 40) echo mb_substr(get_the_title(), 0, 40).$readmore_text; else the_title(); ?></a></h3>
+					<p class="side-meta"><span class="folder"><?php if (get_the_category()) the_category(', '); else echo get_the_term_list($post->ID, 'ad_cat', '', ', ', ''); ?></span> <?php if(get_post_meta(get_the_ID(), 'cp_ad_sold', true) == 'yes') : echo apply_filters('sticky_featured_sold_text', ' '); else: ?>| <?php echo cp_get_price(get_the_ID(), 'cp_price'); ?>				<?php endif; ?>																							<!--<?php if(get_post_meta(get_the_ID(), 'price', true)) cp_get_price_legacy(get_the_ID()); else cp_get_price(get_the_ID(), 'cp_price'); ?></p>-->
+					<p><?php echo mb_substr(strip_tags(get_the_content()), 0, 80).$readmore_text;?></p>
+				</li>
+				<?php
+			else :
 				echo '<li>';
 				if(function_exists('cp_ad_featured_thumbnail') && $showthumbs) cp_ad_featured_thumbnail();
 				elseif (has_post_thumbnail() && $showthumbs){
 					echo '<a href="' . get_permalink() . '">';
 					the_post_thumbnail(array(get_option('thumbnail_size_w'),get_option('thumbnail_size_h')), array('style' => 'float: left; width: 50px; height: 50px; margin-right: 5px;'));
 					echo '</a>';
-				}		
+				}
 				echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
 				echo '<p style="margin: 0px;">'.mb_substr(strip_tags(get_the_content()), 0, 160).$readmore_text.'</p>';
 				echo '<br style="clear: both;" />';
@@ -124,59 +143,57 @@ class WP_Widget_smSticky extends WP_Widget {
 		echo '</ul>';
 		//End of The Loop
 		echo $after_widget;
-		
+
 		//debug printing
 		//echo 'query_vars <pre>'; print_r($smStickyPosts->query_vars); echo '</pre>';
 		//echo '$queryArrayOrString <pre>'; print_r($queryArrayOrString); echo '</pre>';
 		//echo '$term <pre>'; print_r($term); echo '</pre>';
 		wp_reset_query();
 	} // End function widget.
-	
-	
-	
+
+
+
 	// Updates the settings.
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		/* Strip tags (if needed) and update the widget settings. */
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['number'] = (int) $new_instance['number'];
-        $instance['showthumbs'] = isset($new_instance['showthumbs']) ? 1 : 0;
-        $instance['catTitles'] = isset($new_instance['catTitles']) ? 1 : 0;
+		$instance['showthumbs'] = isset($new_instance['showthumbs']) ? 1 : 0;
+		$instance['catTitles'] = isset($new_instance['catTitles']) ? 1 : 0;
 		$instance['showType'] = $new_instance['showType'];
 		return $instance;
 	} // End function update
-	
-	
-	
+
+
+
 	// The admin form.
-	function form($instance) {	
-	$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-	if ( !isset($instance['number']) || !$number = (int) $instance['number'] ) $number = 5;
-?>
-<div id="smSticky-admin-panel">
-    <p><label for="<?php echo $this->get_field_id("title"); ?>">Title:</label>
-    <input type="text" class="widefat" name="<?php echo $this->get_field_name("title"); ?>" id="<?php echo $this->get_field_id("title"); ?>" value="<?php echo $instance["title"]; ?>" /></p>
+	function form($instance) {
+		//setup values in cases where no values exist yet (ie upon creation)
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
+		?>
+		<div id="smSticky-admin-panel">
+			<p><label for="<?php echo $this->get_field_id("title"); ?>">Title:</label>
+				<input type="text" class="widefat" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" value="<?php echo $instance['title']; ?>" /></p>
 
-	<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
-	<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+			<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
+				<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $instance['number'] ?>" size="3" /></p>
 
-    <p><input class="checkbox" type="checkbox" <?php checked( $instance['showthumbs'], true ); ?> id="<?php echo $this->get_field_id( 'showthumbs' ); ?>" name="<?php echo $this->get_field_name('showthumbs'); ?>" />
-    <label for="<?php echo $this->get_field_id('showthumbs'); ?>"> Display Thumbnails?</label></p>
-    
-    <p><input class="checkbox" type="checkbox" <?php checked( $instance['catTitles'], true ); ?> id="<?php echo $this->get_field_id( 'catTitles' ); ?>" name="<?php echo $this->get_field_name('catTitles'); ?>" />
-    <label for="<?php echo $this->get_field_id('catTitles'); ?>"> Only show sticky posts of the post category & sub categories?</label></p>
-	
-    <p><label for="<?php echo $this->get_field_id('showType'); ?>"> Posts to display: </label>
-    	<select class="dropdown" id="<?php echo $this->get_field_id( 'showType' ); ?>" name="<?php echo $this->get_field_name('showType'); ?>">
-    		<option value="sticky" <?php selected( $instance['showType'], 'sticky' ); ?>>Sticky</option>
-            <option value="all" <?php selected( $instance['showType'], 'all' ); ?>>All</option>
-            <option value="reg" <?php selected( $instance['showType'], 'reg' ); ?>>Non-Sticky</option>
-        </select></p>
-    </div>
-    
-<?php
+			<p><input class="checkbox" type="checkbox" <?php checked( $instance['showthumbs'], true ); ?> id="<?php echo $this->get_field_id( 'showthumbs' ); ?>" name="<?php echo $this->get_field_name('showthumbs'); ?>" />
+				<label for="<?php echo $this->get_field_id('showthumbs'); ?>"> Display Thumbnails?</label></p>
+
+			<p><input class="checkbox" type="checkbox" <?php checked( $instance['catTitles'], true ); ?> id="<?php echo $this->get_field_id( 'catTitles' ); ?>" name="<?php echo $this->get_field_name('catTitles'); ?>" />
+				<label for="<?php echo $this->get_field_id('catTitles'); ?>"> Only show sticky posts of the post category & sub categories?</label></p>
+
+			<p><label for="<?php echo $this->get_field_id('showType'); ?>"> Posts to display: </label>
+				<select class="dropdown" id="<?php echo $this->get_field_id( 'showType' ); ?>" name="<?php echo $this->get_field_name('showType'); ?>">
+					<option value="sticky" <?php selected( $instance['showType'], 'sticky' ); ?>>Sticky</option>
+					<option value="all" <?php selected( $instance['showType'], 'all' ); ?>>All</option>
+					<option value="reg" <?php selected( $instance['showType'], 'reg' ); ?>>Non-Sticky</option>
+				</select></p>
+		</div>
+		<?php
 	} // end function form
 
 } // end class WP_Widget_smSticky
 
-?>
